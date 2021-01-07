a582c4f6dadc01f63ba281dafd01822ea9ad0e90
Loki2 <stevloki@ymail.com>
2021-01-07 09:51:25 +0700

    Backend Uri update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b1c0f7041ba761cdc61c";
+/******/ 	var hotCurrentHash = "7300f92d64d5980168de";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
